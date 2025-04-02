8386ef349c0d638125248965ab3a359ded6c2dcb
Blaise Niyonkuru <62561244+Blaiseniyo@users.noreply.github.com>
2025-04-02 20:56:25 +0200

    Add BFF

---
+++ src/features/books/services.ts
@@ -0,0 +1,41 @@
+/*
+services.ts
+description: This file contains the services for books, using axios to forward requests to the Book Service
+author: Blaise Niyonkuru<blaiseniyonkuru12@gmail.com>
+*/
+
+import Book from "../../database/models/book";
+import apiClient from '../../utils/axois';
+import { CreateBookInput, UpdateBookInput } from './type';
+
+type CustomBookData = Omit<Book, 'genre'> & { genre: number };
+
+export class BookService {
+    async createBook(bookData: CreateBookInput['body']) {
+        const response = await apiClient.post('/books', bookData);
+        return response.data as Book;
+    }
+
+    async updateBook(params: UpdateBookInput['params'], bookData: UpdateBookInput['body']) {
+        const response = await apiClient.put(`/books/${params.ISBN}`, bookData);
+        return response.data as Book;
+    }
+
+    async getBookByISBN(ISBN: string) {
+
+        const response = await apiClient.get(`/books/${ISBN}`);
+        const bookData = response.data as Book;
+
+        // Transform genre value: replace "non-fiction" with numeric value 3
+
+        // Check if genre field exists and equals "non-fiction"
+        if (bookData && bookData.genre === "non-fiction") {
+            const transformedBookData = { ...bookData, genre: 3 } as CustomBookData;
+            return transformedBookData;
+        }
+
+        return bookData;
+    }
+}
+
+export default new BookService();
---
+++ src/features/customers/services.ts
@@ -0,0 +1,44 @@
+/*
+services.ts
+description: This file contains the services for customers, using axios to forward requests to the Customer Service
+author: Blaise Niyonkuru<blaiseniyonkuru12@gmail.com>
+*/
+
+import Customer from "../../database/models/customer";
+import apiClient from '../../utils/axois';
+import { CreateCustomerInput } from './types';
+
+export class CustomerService {
+  async createCustomer(customerData: CreateCustomerInput['body']) {
+    const response = await apiClient.post('/customers', customerData);
+    return response.data as Customer;
+  }
+
+  async getCustomerById(id: number) {
+    const response = await apiClient.get(`/customers/${id}`);
+    const customer = response.data as Customer;
+
+    // Remove address-related attributes from the response
+    if (response.data) {
+      const { address, address2, city, state, zipcode, createdAt, updatedAt, ...sanitizedCustomer } = customer;
+      return sanitizedCustomer;
+    }
+
+    return response.data;
+  }
+
+  async getCustomerByUserId(userId: string) {
+    const response = await apiClient.get(`/customers?userId=${userId}`);
+    const customer = response.data as Customer;
+
+    // Remove address-related attributes from the response
+    if (response.data) {
+      const { address, address2, city, state, zipcode, createdAt, updatedAt, ...sanitizedCustomer } = customer;
+      return sanitizedCustomer;
+    }
+
+    return response.data;
+  }
+}
+
+export default new CustomerService();
